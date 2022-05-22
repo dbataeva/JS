e4255fe0c55e0ef1e3591078b0f549e2599d68b7
dbataeva <b.daffa.v@yandex.ru>
2022-05-22 12:41:41 +0300

    final

--- clock/animation.js
+++ clock/animation.js
@@ -1,29 +1,12 @@
-function rotateSecondHand() {
-	let date = new Date();
-	let secDegree = date.getSeconds() * 6 + howMuchTimesSec + 90; 
-	let minDegree = date.getMinutes() * 6 + 90 + 6 / 60 * date.getSeconds();
-	let hourDegree = (date.getHours() - 12) * 30 + 90 + 30 / 60 * date.getMinutes();
-
+function rotateHands() {
+	secDegree += 6;
 	second.style.transform = `rotate(${secDegree}deg)`;
+
+	minDegree += 0.1;
 	minute.style.transform = `rotate(${minDegree}deg)`;
+
+	hourDegree += 0.08333333;
 	hour.style.transform = `rotate(${hourDegree}deg)`;
-}
-
-function addHowMuchTimesSec() {
-	howMuchTimesSec += 360;
-}
-
-function addHowMuchTimesSecOnce() {
-	howMuchTimesSec += 360;
-	setInterval(addHowMuchTimesSec, 1000 * 60);
-}
-
-function addHowMuchTimesMin() {
-	howMuchTimesMin += 1;
-}
-
-function addHouMuchTimesHour() {
-	howMuchTimesHour += 1;
 }
 
 const hour = document.querySelector('div[data-hand="hour"]');
@@ -31,24 +14,23 @@ const minute = document.querySelector('div[data-hand="minute"]');
 const second = document.querySelector('div[data-hand="second"]');
 
 let date = new Date();
-let secDegree = date.getSeconds() * 6 + 90;
-let minDegree = date.getMinutes() * 6 + 90;
-let hourDegree = (date.getHours() - 12) * 30 + 90 + 30 / 60 * date.getMinutes();
-let howMuchTimesSec = 0;
-let howMuchTimesMin = 1;
-let howMuchTimesHour = 1;
-let diff;
+let sec = date.getSeconds();
+let secDegree = sec * 6 + 90;
+let min = date.getMinutes();
+let minDegree = min * 6 + 90 + 6 / 60 * sec;
+let hourDegree = (date.getHours() - 12) * 30 + 90 + 30 / 60 * min;
 
 if (secDegree > 360) {
-	diff = secDegree - 360;
-} else {
-	diff = 360 - secDegree;
+	secDegree -= 360;
+}
+if (minDegree > 360) {
+	minDegree -= 360;
+}
+if (hourDegree > 360) {
+	hourDegree -= 360;
 }
 second.style.transform = `rotate(${secDegree}deg)`;
 minute.style.transform = `rotate(${minDegree}deg)`;
 hour.style.transform = `rotate(${hourDegree}deg)`;
 
-console.log(diff);
-
-setTimeout(addHowMuchTimesSecOnce, 1000 * diff);
-setInterval(rotateSecondHand, 1000);
+setInterval(rotateHands, 1000);
